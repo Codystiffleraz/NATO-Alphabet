fb5befe23d152aa8a81cffc4c47c43e6b4e2cc87
Cody Stiffler <codystiffleraz@gmail.com>
2023-11-03 12:49:47 -0700

    added exception handling

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # Converts DataFrame to dict in {A: "Alpha"} format
 words = {row.letter:row.code for (index, row) in data.iterrows()}    
-# User input for a word
-user_input = input("Type a word :").upper()
-# Pulls the value from the key and adds it to a list from the typed user input
-new_list = [words[letter] for letter in user_input]
-print(new_list)
+
+
+def generate_phonetic():
+    # User input for a word
+    user_input = input("Type a word: ").upper()
+    try:
+        # Pulls the value from the key and adds it to a list from the typed user input
+        new_list = [words[letter] for letter in user_input]
+    except KeyError:
+        # Validates the input is a letter if not prints type a letter and repeats the input
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(new_list)
+
+generate_phonetic()
